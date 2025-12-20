4a491883a74cf075b161e4ad3d455d6e5870b54d
lukas söderlund <ls224ec@student.lnu.se>
2025-12-20 23:55:50 +0100

    kasd

--- fake-casino/server.js
+++ fake-casino/server.js
@@ -5,19 +5,29 @@ const connectDB = require('./config/db')
 const app = require('./app')
 
 const server = http.createServer(app)
-const io = new Server(server)
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  },
+  path: '/socket.io/'
+})
 
 connectDB()
 
 // Store io in app so controllers can access it
 app.set('io', io)
 
+// Share session with Socket.io
+const sessionMiddleware = require('./config/session')
+io.engine.use(sessionMiddleware)
+
 // Socket handlers
 require('./sockets/chatSocket')(io)
 require('./sockets/gameSocket')(io)
 require('./sockets/lobbySocket')(io)
 require('./sockets/rouletteSocket')(io)
-require('./sockets/levelSocket')(io)
+require('./sockets/levelSocket')(io) 
 
 const PORT = process.env.PORT || 3000
 
